b4c88227e7945dd6ccc50ddd38cc3ba04dcad040
Josh <josh@joshthomas.dev>
2023-09-20 09:28:18 -0500

    update test settings

--- tests/settings.py
+++ tests/settings.py
@@ -1,22 +1,29 @@
 from __future__ import annotations
 
+from email_relay.conf import EMAIL_RELAY_DATABASE_ALIAS
+
 ALLOWED_HOSTS = ["*"]
 
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": ":memory:",
-    }
+    },
+    EMAIL_RELAY_DATABASE_ALIAS: {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": ":memory:",
+    },
 }
+
+DATABASE_ROUTERS = [
+    "email_relay.db.EmailRelayDatabaseRouter",
+]
+
 
 EMAIL_BACKEND = "email_relay.backend.DatabaseEmailBackend"
 
 INSTALLED_APPS = [
-    "django.contrib.auth",
-    "django.contrib.contenttypes",
-    "django.contrib.sessions",
     "email_relay",
-    "tests",
 ]
 
 SECRET_KEY = "NOTASECRET"
