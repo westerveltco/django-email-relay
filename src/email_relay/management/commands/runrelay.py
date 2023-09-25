135f3bf4c869b9b8b6e348f424d19f7fa85a39dc
Josh <josh@joshthomas.dev>
2023-09-25 11:04:40 -0500

    add more logging to relay

--- src/email_relay/management/commands/runrelay.py
+++ src/email_relay/management/commands/runrelay.py
@@ -14,14 +14,16 @@ logger = logging.getLogger(__name__)
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
+        logger.info("starting relay")
         while True:
             while (
                 not Message.objects.queued().exists()
                 and not Message.objects.deferred().exists()
             ):
-                logger.debug(
-                    f"sleeping for {app_settings.EMPTY_QUEUE_SLEEP} seconds before checking queue again"
-                )
-                time.sleep(app_settings.EMPTY_QUEUE_SLEEP)
+                msg = "queue is empty"
+                if app_settings.EMPTY_QUEUE_SLEEP > 0:
+                    msg += f", sleeping for {app_settings.EMPTY_QUEUE_SLEEP} seconds before checking queue again"
+                    time.sleep(app_settings.EMPTY_QUEUE_SLEEP)
+                logger.debug(msg)
 
             send_all()
