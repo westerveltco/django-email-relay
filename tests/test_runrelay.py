1dcd1bd19607d18b4051659152da4449f23a57ac
Jeff Triplett <jeff.triplett@gmail.com>
2023-09-25 15:45:06 -0500

    :green_heart: Adds test_runrelay cmd

---
+++ tests/test_runrelay.py
@@ -0,0 +1,13 @@
+import pytest
+from django.core.management import call_command
+
+
+def test_runrelay_help():
+    # We'll capture the output of the command
+    with pytest.raises(SystemExit) as exec_info:
+        # call_command will execute our command as if we ran it from the command line
+        # the 'stdout' argument captures the command output
+        call_command("runrelay", "--help")
+
+    # Asserting that the command exits with a successful exit code (0 for help command)
+    assert exec_info.value.code == 0
