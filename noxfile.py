0d63c4f47e4e148493376ac88a2b342f2b233b60
Jeff Triplett <jefftriplett@users.noreply.github.com>
2023-09-26 16:54:41 -0500

    :sparkles: Use nox with GitHub Actions (#27)
    * :sparkles: Use nox with GitHub Actions
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * :pencil: Fixes missed comma
    
    * :pencil: Adds caching
    
    * :tractor: :fire: Remove extra pip install
    
    * :tractor: Call mypy via python module name
    
    * :tractor: Refactors lint to run via python module
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- noxfile.py
+++ noxfile.py
@@ -1,5 +1,8 @@
 from __future__ import annotations
 
+import os
+from pathlib import Path
+
 import nox
 
 PY38 = "3.8"
@@ -56,17 +59,38 @@ def tests(session, django):
 def coverage(session):
     session.install(".[dev]")
     session.run("python", "-m", "pytest", "--cov=email_relay")
-    session.run("python", "-m", "coverage", "html", "--skip-covered", "--skip-empty")
+
+    try:
+        summary = os.environ["GITHUB_STEP_SUMMARY"]
+        with Path(summary).open("a") as output_buffer:
+            output_buffer.write("")
+            output_buffer.write("### Coverage\n\n")
+            output_buffer.flush()
+            session.run(
+                "python",
+                "-m",
+                "coverage",
+                "report",
+                "--skip-covered",
+                "--skip-empty",
+                "--format=markdown",
+                stdout=output_buffer,
+            )
+    except KeyError:
+        session.run(
+            "python", "-m", "coverage", "html", "--skip-covered", "--skip-empty"
+        )
+
     session.run("python", "-m", "coverage", "report")
 
 
 @nox.session
 def lint(session):
     session.install(".[lint]")
-    session.run("pre-commit", "run", "--all-files")
+    session.run("python", "-m", "pre_commit", "run", "--all-files")
 
 
 @nox.session
 def mypy(session):
     session.install(".[dev]")
-    session.run("mypy")
+    session.run("python", "-m", "mypy", ".")
