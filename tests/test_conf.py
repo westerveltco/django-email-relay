d42692a7f5b120a4dc494ff2c5ad0c5df2aa4f81
Josh <josh@joshthomas.dev>
2023-10-27 13:56:07 -0500

    update conf tests with new settings

--- tests/test_conf.py
+++ tests/test_conf.py
@@ -1,42 +1,58 @@
 from __future__ import annotations
 
+import pytest
+
 from django.conf import settings
 from django.test import override_settings
 
 from email_relay.conf import app_settings
 
 
-def test_default_settings():
+@pytest.mark.parametrize(
+    "setting,default_setting",
+    [
+        ("DATABASE_ALIAS", "email_relay_db"),
+        ("EMAIL_MAX_BATCH", None),
+        ("EMAIL_MAX_DEFERRED", None),
+        ("EMAIL_MAX_RETRIES", None),
+        ("EMPTY_QUEUE_SLEEP", 30),
+        ("EMAIL_THROTTLE", 0),
+        ("MESSAGES_BATCH_SIZE", None),
+        ("MESSAGES_RETENTION_SECONDS", None),
+        ("RELAY_HEALTHCHECK_METHOD", "GET"),
+        ("RELAY_HEALTHCHECK_STATUS_CODE", 200),
+        ("RELAY_HEALTHCHECK_TIMEOUT", 5.0),
+        ("RELAY_HEALTHCHECK_URL", None),
+    ],
+)
+def test_default_settings(setting, default_setting):
     user_settings = getattr(settings, "DJANGO_EMAIL_RELAY", {})
 
     assert user_settings == {}
-    assert app_settings.DATABASE_ALIAS == "email_relay_db"
-    assert app_settings.MESSAGES_BATCH_SIZE is None
-    assert app_settings.EMAIL_MAX_RETRIES is None
+    assert getattr(app_settings, setting) == default_setting
 
 
-@override_settings(
-    DJANGO_EMAIL_RELAY={
-        "DATABASE_ALIAS": "custom_db_name",
-    },
+@pytest.mark.parametrize(
+    "setting,user_setting",
+    [
+        ("DATABASE_ALIAS", "custom_db_name"),
+        ("EMAIL_MAX_BATCH", 10),
+        ("EMAIL_MAX_DEFERRED", 10),
+        ("EMAIL_MAX_RETRIES", 10),
+        ("EMPTY_QUEUE_SLEEP", 1),
+        ("EMAIL_THROTTLE", 1),
+        ("MESSAGES_BATCH_SIZE", 10),
+        ("MESSAGES_RETENTION_SECONDS", 10),
+        ("RELAY_HEALTHCHECK_METHOD", "POST"),
+        ("RELAY_HEALTHCHECK_STATUS_CODE", 201),
+        ("RELAY_HEALTHCHECK_TIMEOUT", 10.0),
+        ("RELAY_HEALTHCHECK_URL", "http://example.com/healthcheck"),
+    ],
 )
-def test_custom_database_alias():
-    assert app_settings.DATABASE_ALIAS == "custom_db_name"
-
-
-@override_settings(
-    DJANGO_EMAIL_RELAY={
-        "MESSAGES_BATCH_SIZE": 10,
-    },
-)
-def test_custom_batch_size():
-    assert app_settings.MESSAGES_BATCH_SIZE == 10
-
-
-@override_settings(
-    DJANGO_EMAIL_RELAY={
-        "EMAIL_MAX_RETRIES": 5,
-    },
-)
-def test_custom_max_retries():
-    assert app_settings.EMAIL_MAX_RETRIES == 5
+def test_custom_settings(setting, user_setting):
+    with override_settings(
+        DJANGO_EMAIL_RELAY={
+            setting: user_setting,
+        },
+    ):
+        assert getattr(app_settings, setting) == user_setting
