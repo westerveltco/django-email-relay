325c259d1111ca32492503f0e204fab9c5403d80
Josh <josh@joshthomas.dev>
2025-01-21 12:02:27 -0600

    remove from skip check

--- noxfile.py
+++ noxfile.py
@@ -40,10 +40,6 @@ def should_skip(python: str, django: str) -> bool:
         # Django main requires Python 3.10+
         return True
 
-    if django == DJ32 and version(python) >= version(PY312):
-        # Django 3.2 requires Python < 3.12
-        return True
-
     if django == DJ50 and version(python) < version(PY310):
         # Django 5.0 requires Python 3.10+
         return True
